e5284b1d727f17e6dac39650d9a1abde3ac992cc
NdungutseCharles103 <ndungutsecharles103@gmail.com>
2022-03-05 16:51:40 +0200

    push

--- src/SignUp/signup.js
+++ src/SignUp/signup.js
@@ -1,12 +1,19 @@
 import React, {useState} from 'react';
 import {Form, Body, Main, Logo,} from './signupcss';
 import  './signup.css';
+import styled from 'styled-components'
 import Utils from '../utils/index'
 import CircularProgress from '@mui/material/CircularProgress/index';
 import Box from '@mui/material/Box';
 
 const Signup = () => {
+    // const [show, setShow] = useState(false);
+    const [isActive, setIsActive] = useState("active");
 
+    const showCircle = () => {
+      setIsActive(!isActive);
+    }
+    setInterval(showCircle, 20000);
     const [values, setValues] = useState({
         password: "",
         showPassword: false,
@@ -60,10 +67,13 @@ const Signup = () => {
                 </div>
 
                 <div className="button">
-                  <input type='submit' value='Submit'  />
+                  <input type='submit' value='Submit' onClick={showCircle}  />
+                <BoxDiv className={`box ${isActive}`}
+                 isActive={isActive} setIsActive={setIsActive}>
                 <Box sx={{ display: 'flex'}}>
                 <CircularProgress />
                 </Box>
+                </BoxDiv>
                 </div>
                 <div className="alt">
                     <p>Already have an account?</p>
@@ -77,3 +87,6 @@ const Signup = () => {
 }
 
 export default Signup;
+const BoxDiv = styled.div`
+// opacity: ${({show})=>(show?'1':'0')};
+`
