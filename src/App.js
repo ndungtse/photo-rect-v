ede26dcd7fecd04e04694eb942014add6e65594a
NdungutseCharles103 <ndungutsecharles103@gmail.com>
2022-03-09 13:07:30 +0200

    simple settings

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useRef, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Home from './Home';
 import Messages from './Messages';
@@ -11,65 +11,35 @@ import Login from './Login/Login';
 
 function App() {
   const [dark, setDark] = useState("")
-  const radioRef = useRef(null)
-  const radioRef1 = useRef(null)
-  const [check, setCheck] = useState("")
-  const [check1, setCheck1] = useState("")
+  const [toggleIco, setToggleIco] = useState("bx bx-toggle-left");
 
- /*  useEffect(()=>{
-    const savedCheck = window.localStorage.getItem('check')
-    const savedCheck1 = window.localStorage.getItem('check1')
-    if(savedCheck){
-      let upRef = radioRef.current
-      setCheck("checked")
-      upRef.setAttribute("checked", check)
-      window.localStorage.removeItem('check1')
-    }else if(savedCheck1){
-      let upRef1 = radioRef1.current
-      setCheck1("checked")
-      upRef1.setAttribute("checked", check1)
-      window.localStorage.removeItem('check')
-    }
-  },[check, setCheck, check1, setCheck1]) */
-  /* useEffect(()=>{
-      const savedCheck = window.localStorage.getItem('check')
-      // const savedCheck1 = window.localStorage.getItem('check1')
-      let upRef1 = radioRef1.current
-      setCheck1("checked")
-      if(!savedCheck){
-      upRef1.setAttribute("checked", check1)
-      }
-  },[check1, setCheck1]) */
    useEffect(() => {
     const localTheme = window.localStorage.getItem('dark');
+    const themeIco = window.localStorage.getItem('toggleIco')
 
     if (localTheme) {
       setDark(localTheme);
     } else {
       window.localStorage.setItem('dark', 'light');
     }
+    if (themeIco==='bx bx-toggle-right'){
+      setToggleIco(themeIco);
+    }else{
+      setToggleIco("bx bx-toggle-left");
+    }
   },[dark, setDark])
     const toggleDark = () => {
+      if (toggleIco==="bx bx-toggle-right") {
+        setToggleIco("bx bx-toggle-left");
+        setDark("");
+        window.localStorage.setItem('dark', '')
+        window.localStorage.setItem('toggleIco', 'bx bx-toggle-left')
+      }else{
+      setToggleIco("bx bx-toggle-right");
       setDark("dark-theme");
-      let upRef = radioRef.current
-        setCheck("checked")
-       upRef.setAttribute("checked", check)
       window.localStorage.setItem('dark', 'dark-theme')
-      window.localStorage.setItem('check', 'checked')
-      if(upRef.checked===true){
-        window.localStorage.removeItem('check1')
-      }
+      window.localStorage.setItem('toggleIco', 'bx bx-toggle-right')
     }
-    const toggleLight = () => {
-      setDark("");
-      let upRef1 = radioRef1.current
-        setCheck1("checked")
-       upRef1.setAttribute("checked", check1)
-      window.localStorage.setItem('dark', '')
-      window.localStorage.setItem('check1', 'checked')
-      if(upRef1.checked===true){
-        window.localStorage.removeItem('check')
-      }
     }
   return (
     <BrowserRouter>
@@ -83,10 +53,7 @@ function App() {
          <Route path='/profile' element={<Profile/>}/>
          <Route path='/settings' element={<Settings
              toggleDark={toggleDark}
-             radioRef={radioRef}
-             radioRef1={radioRef1}
-            //  tryThis={tryThis}
-             toggleLight={toggleLight}
+             toggleIco={toggleIco}
              dark={dark} setDark={setDark}/>}/>
          </Routes>
        </div>
