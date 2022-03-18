ca15c35f0f2be8f4be16bf62ff47bfe9d83462be
precieux33 <precieuxmugisha@gmail.com>
2022-03-18 09:32:16 +0200

    connected to front-end

--- src/settings/deleteAccount.js
+++ src/settings/deleteAccount.js
@@ -1,23 +1,22 @@
-let userName,password
+let userName, password
 const getuname = e => {
     userName = e.target.value
-    console.log(userName)
 }
 const getpswd = e => {
     password = e.target.value
-    console.log(password)
 }
 const onload = e => {
     e.preventDefault()
 }
 const onsubmit = e => {
     e.preventDefault()
-    fetch("http://photocorner33.herokuapp.com/user/deleteUser", {
+    fetch("http://localhost:5000/user/deleteUser", {
         method: "POST",
         // mode: "no-cors",
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
-            userName, password
+            userName: (userName),
+            password: (password)
         })
     }).then(res => res.json())
         .then(data => {
@@ -28,8 +27,11 @@ const onsubmit = e => {
                 localStorage.removeItem("fullName")
                 window.location.replace('http://photo-rect-v.vercel.app/signup')
             }
-            else {
-                window.alert("⚠️⚠️⚠️Either your username or password is incorrect try again!!⚠️⚠️⚠️")
+            else if (data.message === "ACCOUNT NOT DELETED") {
+                window.alert("⚠️⚠️⚠️Password is incorrect try again!!⚠️⚠️⚠️")
+            }
+            else if (data.message === "USERNAME INCORRECT") {
+                window.alert("⚠️⚠️⚠️Type the username correctly please!!⚠️⚠️⚠️")
             }
         })
 }
