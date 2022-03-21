6d14e0503a5a66f9b10faef6cfb60e22fadc876c
precieux33 <precieuxmugisha@gmail.com>
2022-03-21 18:00:46 +0200

    wow this is getting harder and harder we can get form results but ther is an error

--- src/account-page/updateAccount.js
+++ src/account-page/updateAccount.js
@@ -1,9 +1,21 @@
 // eslint-disable-next-line no-unused-vars
-let previousUserName, fullName, userName, email, password, bio
+let previousUserName, fullName, userName, email, password, bio, userInfo
 previousUserName = localStorage.getItem("userName")
-const onload = e => {
-    // e.preventDefault()
+const onload = async (e) => {
+    return new Promise((resolve, reject)=>{
+        fetch('http://localhost:5000/user/loggedUser', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                previousUserName: (previousUserName)
+            })
+        }).then(res => res.json())
+            .then(data => {
+                resolve(data)
+            })
+    })
 }
+
 const onSubmit = e => {
     e.preventDefault()
     email = document.querySelector("#email").value
@@ -11,12 +23,12 @@ const onSubmit = e => {
     userName = document.querySelector("#username").value
     password = document.querySelector("#password").value
     bio = document.querySelector("#bio").value
-    console.log(email,fullName,userName,password,bio)
-    fetch('http://photocorner33.herokuapp.com/user/updateUser', {
+    console.log(previousUserName)
+    fetch('http://localhost:5000/user/updateUser', {
         method: "POST",
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
-            previousUserName: (previousUserName),
+            previousUserName:(previousUserName),
             fullName: (fullName),
             userName: (userName),
             email: (email),
@@ -26,9 +38,9 @@ const onSubmit = e => {
         .then(data => {
             console.log(data)
             if (data.message === "USER UPDATED") {
-                console.log(userName,fullName);
-                (userName === "")?console.log("USERNAME NOT UPDATED") : localStorage.setItem("userName",userName);
-                (fullName === "")?console.log("FULLNAME NOT UPDATED") : localStorage.setItem("fullName",fullName);
+                console.log(userName, fullName);
+                (userName === "") ? console.log("USERNAME NOT UPDATED") : localStorage.setItem("userName", userName);
+                (fullName === "") ? console.log("FULLNAME NOT UPDATED") : localStorage.setItem("fullName", fullName);
                 window.alert("ACCOUNT UPDATED SUCCESSFULLY")
             }
             else if (data.message === "No user found") {
