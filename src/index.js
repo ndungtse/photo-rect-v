b2a837bb4563fa7f2390cc8544b1b323e0743192
NdungutseCharles103 <ndungutsecharles103@gmail.com>
2022-03-26 16:03:29 +0200

    login rule

--- src/index.js
+++ src/index.js
@@ -3,19 +3,10 @@ import ReactDOM from 'react-dom';
 import 'boxicons'
 import './index.css';
 import App from './App';
-import Login from "./Login/Login";
-import reportWebVitals from './reportWebVitals';
-
-let userName = localStorage.getItem("userName");
 
 ReactDOM.render(
   <React.StrictMode>
-    {userName ? <App/>:<Login />}
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
