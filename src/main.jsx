95e11001df5fdd5b7ba0113acca73d9ce497560e
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2022-06-06 20:33:26 +0200

    vercel.json for vite

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 	<UserProvider>
    	  <App />
-    	</UserProvider>
+    </UserProvider>
   </React.StrictMode>
 )
