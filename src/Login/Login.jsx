98828e6aa19f42fa57ea352ac811b5e3bf43b31d
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2022-06-08 16:54:18 +0200

    forms

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -6,7 +6,7 @@ import './../SignUp/signup.css'
 import Utils from '../utils';
 import loginUtils from './index';
 import Checkbox from '@mui/material/Checkbox';
-
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
@@ -31,37 +31,43 @@ const Login = () => {
 
   return (
     <>
-      <Body className='loback'>
+      <Body className='loback text-black'>
         <Main>
-          <Form className='shadow-lg forform' onLoad={Utils.onload} onSubmit={loginUtils.onloginsubmit}>
+          <Form className=' bg-slate-200 text-black' onLoad={Utils.onload} onSubmit={loginUtils.onloginsubmit}>
             <Logo><img src="./src/Home/Images/logo.png" alt="logo" /></Logo>
-            <h1>Log into Photo Corner</h1>
-            <div className="labels">
-              <TextField className="inmaterial inlog" label="Email"
-          id="standard-input" variant="filled" onInput={loginUtils.getemail} type="email" placeholder="Enter your email" autoComplete="off" required/>
+            <h1 className='text-black'>Log into Photo Corner</h1>
+            <div className="w-full flex items-center justify-center">
+              <TextField
+                sx={{marginTop: 2, width: '100%', maxWidth: 300}}
+                size="small"
+                className="" label="Email"
+                id="standard-input" variant="outlined"
+                 onInput={loginUtils.getemail} type="email" placeholder="Enter your email" autoComplete="off" required/>
             </div>
-            <div className="labels">
-              <TextField className="inmaterial inlog" name='email' placeholder="Enter your passoward"
-          id="standard-password-input" variant="filled"
-          label="Password" onInput={loginUtils.getpswd} type={values.showPassword ? "text" : "password"}
+            <div className="w-full flex items-center justify-center">
+              <TextField className="mt-4"
+              size="small" sx={{marginTop: 2, width: '100%', maxWidth: 300}}
+               name='email' placeholder="Enter your passoward"
+              id="standard-password-input" variant="outlined"
+               label="Password" onInput={loginUtils.getpswd} type={values.showPassword ? "text" : "password"}
                 onChange={handlePasswordChange("password")}
                 value={values.password} required />
             </div>
             <div className="label">
             <Checkbox {...label} onClick={handleShowPassword}
                 onMouseDown={handleMouseDownPassword} type="checkbox" />
-              <label>Show password</label>
+              <label className='text-black'>Show password</label>
             </div>
-            <div className="button">
+            <div className="bg-blue-600 p-2 px-5 cursor-pointer">
               <input type='submit' value='Login' />
             </div>
 
-            <div className="alt1 ">
+            <div className="text-black ">
               <a href="./">Forgot your password?</a>
             </div>
-            <div className="alt">
+            <div className="flex w-full text-black items-center justify-center">
               <p>Don't have an account?</p>
-              <a href="./signup">Sign Up</a>
+              <Link to='/signup' className='text-blue-600 ml-2'>Sign Up</Link>
             </div>
           </Form>
         </Main>
--- src/SignUp/signup.jsx
+++ src/SignUp/signup.jsx
@@ -4,6 +4,7 @@ import  './signup.css';
 import Utils from '../utils/index'
 import Checkbox from '@mui/material/Checkbox';
 import TextField from '@mui/material/TextField';
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
   const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
@@ -24,47 +25,67 @@ const Signup = () => {
       const handlePasswordChange = (prop) => (event) => {
         setValues({ ...values, [prop]: event.target.value });
       };  
+
+      //            sx={{marginTop: 2, width: '100%', maxWidth: 300}}
+      //           size="small"
+      //           id="standard-input" variant="outlined"
   return (  
   <>
       <Body className='loback'>
         <Main>
-            <Form onLoad={Utils.onload} onSubmit={Utils.onsubmit}  className='shadow-lg forform'>
+            <Form onLoad={Utils.onload} onSubmit={Utils.onsubmit}  className='shadow-lg bg-slate-200'>
                 <Logo ><img src="./src/Home/Images/logo.png" alt="logo" /></Logo>
-                <h1>Sign Up to Photo Corner</h1>
-                <div className="labels">
-                    <TextField className="inmaterial insign" label="Full Name"
-          id="standard-password-input" variant="filled" onInput={Utils.getfullname} type="text" placeholder="First Name"required />
+                <h1 className='text-black'>Sign Up to Photo Corner</h1>
+                <div className="w-full flex items-center justify-center">
+                    <TextField 
+                    sx={{marginTop: 2, width: '100%', maxWidth: 300}}
+                     size="small"
+                     id="standard-input" variant="outlined"
+                    className="inmaterial insign" label="Full Name"
+                       onInput={Utils.getfullname} type="text" placeholder="First Name"required />
                 </div>
-                <div className="labels">
-                    <TextField className="inmaterial insign" label="username"
-          id="standard-password-input" variant="filled" onInput={Utils.getuname} type="text" placeholder="Enter your username" required />
+                <div className="w-full flex items-center justify-center">
+                    <TextField className="inmaterial insign" 
+                    sx={{marginTop: 2, width: '100%', maxWidth: 300}}
+                    size="small"
+                    id="standard-input" variant="outlined"
+                    label="username"
+                       onInput={Utils.getuname} type="text" placeholder="Enter your username" required />
                 </div>
-                <div className="labels">
-                    <TextField className="inmaterial insign" label="Email"
-          id="standard-password-input" variant="filled" onInput={Utils.getemail} type="email" placeholder="Enter your email" required/>
+                <div className="w-full flex items-center justify-center">
+                    <TextField className="inmaterial insign" 
+                    sx={{marginTop: 2, width: '100%', maxWidth: 300}}
+                    size="small"
+                    id="standard-input" variant="outlined"
+                    label="Email"
+                       onInput={Utils.getemail} type="email" placeholder="Enter your email" required/>
                 </div>
-                <div className="labels">
-                    <TextField className="inmaterial insign" label="Password"
-          id="standard-password-input" variant="filled" onInput={Utils.getpswd} type={values.showPassword ? "text" : "password"}
+                <div className="w-full flex items-center justify-center">
+                    <TextField
+                    sx={{marginTop: 2, width: '100%', maxWidth: 300}}
+                    size="small"
+                    id="standard-input" variant="outlined"
+                     className="inmaterial insign" label="Password"
+                       onInput={Utils.getpswd} type={values.showPassword ? "text" : "password"}
                     onChange={handlePasswordChange("password")}
                     value={values.password}  placeholder="Choose your password" required/>
                 </div>
                 <div className="label">
                 <Checkbox {...label}  onClick={handleClickShowPassword}
               onMouseDown={handleMouseDownPassword} type="checkbox"/>
-                    <label>Show password</label>
+                    <label className='text-black'>Show password</label>
                 </div>
-                <div className="label">
+                <div className="label text-black">
                     <Checkbox {...label} type="checkbox" required/>
                     <label>I agree with terms and conditions</label>
                 </div>
 
-                <div className="button">
-                  <input className='signsub bg-blue-500' type='submit' value='Submit' onSubmit={Utils.onsubmit} />
+                <div className="bg-blue-600 p-2 px-5 cursor-pointer">
+                  <input className='' type='submit' value='Submit' onSubmit={Utils.onsubmit} />
                 </div>
-                <div className="alt">
+                <div className="flex w-full text-black items-center justify-center">
                     <p>Already have an account?</p>
-                    <a href="./login">  Log in</a>
+                    <Link to='/login' className='text-blue-600 ml-6'>  Log in</Link>
                 </div>
             </Form>
         </Main>
