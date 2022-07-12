b7639ca6d92ae202a4d7e2a7c17bdd268c6e7f47
ndungutsecharles103.com <ndungutsecharles103@gmail.com>
2022-07-12 14:14:03 +0200

    postform

--- client/src/Home/main.jsx
+++ client/src/Home/main.jsx
@@ -6,7 +6,7 @@ import Mainconts from "./mainconts";
 function Main() {
   return (
     
-    <div className="main-side">
+    <div className="main-side w-full">
       <div className="main w-full h-screen">
         <Stories />
         <Mainconts />
--- client/src/Home/mainconts.jsx
+++ client/src/Home/mainconts.jsx
@@ -7,14 +7,20 @@ import Side from "./side";
 import { getCookie } from "../contexts/RequireAuth";
 import Post from "./Post";
 import { usePosts } from "../contexts/PostContext";
+import { BiImageAdd, BiPhotoAlbum, BiX } from "react-icons/bi";
 
 function Mainconts() {
 	const { posts } = usePosts();
-
+	const [showPostForm, setShowPostForm] = useState(false);
 
 	return (
 		<div className="flex">
+			{showPostForm ? <PostForm  setShowPostForm={setShowPostForm} /> : null}
 			<div className="w-full h-[86vh] overflow-auto flex flex-col items-center">
+					<div className="">
+						<button onClick={()=> setShowPostForm(true)} className="text-white bg-blue-700 px-4 py-2
+						cursor-pointer mt-4">Add New Post</button>
+					</div>
 					<>
 						<div className="w-full flex flex-col items-center">
 							{posts.map((item) => (
@@ -30,4 +36,43 @@ function Mainconts() {
 
 export default Mainconts;
 
+const PostForm = ({setShowPostForm}) =>{
 
+	const [imageString, setImageStr] = useState('');
+
+	const previewFile = (e) => {
+		const file = e.target.files[0];
+		console.log(file);
+		const reader = new FileReader();
+		reader.addEventListener('loadend',() => {
+			setImageStr(reader.result);
+		});
+		reader.readAsDataURL(file);
+		console.log(reader.result);
+	}
+	return(
+		<div className="w-full top-0 left-0 z-[10] absolute bg-black/70 
+		flex flex-col items-center justify-center h-screen">
+			<div onClick={()=> setShowPostForm(false)}
+			 className="w-full h-screen absolute top-0 left-0 z-[15]"></div>
+			<div className="flex z-20 flex-col relative w-1/3 rounded-xl p-4 min-w-[300px] bg-white">
+				
+					<BiX onClick={()=> setShowPostForm(false)} 
+					 className="text-4xl absolute top-1 hover:text-red-600 cursor-pointer right-3" />
+				<h2 className="text-center text-xl">Post Something</h2>
+				<textarea className="border-2 h-[20vh] outline-none border-blue-500/70
+				  p-2 w-full " placeholder="What's on your mind?" maxLength={700}>
+
+				</textarea>
+				<div className="w-full mt-4 flex items-center justify-between">
+					<label htmlFor="post" className="flex cursor-pointer text-blue-700 items-center">
+						<BiImageAdd className="text-3xl" />
+						<p>Add A Photo</p>
+					</label>
+					<input onChange={previewFile}  className="hidden" type="file" id="post" accept="image/*" />
+					<button className="px-4 py-2 bg-blue-600 text-white">Post</button>
+				</div>
+			</div>
+		</div>
+	)
+}
